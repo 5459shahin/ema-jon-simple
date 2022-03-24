54cb695bd0bcc88bcc1989c628d961baf1407dde
545n shahin <mohammadshahinkhan1992@gmail.com>
2022-03-24 22:24:12 +0800

    added a component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Cart = ({cart}) => {
+    // console.log(cart)
+    return (
+        <div>
+             <h4> order summary</h4>
+             <p>Selected Items: {cart.length}</p>
+        </div>
+    );
+};
+
+export default Cart;
